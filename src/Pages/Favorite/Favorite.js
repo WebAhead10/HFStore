3a7cb56c1e3c7ae60f13e6717a10ef8424925a94
hhasona <hhasona@gmail.com>
2021-07-28 16:35:04 +0300

    up

--- src/Pages/Favorite/Favorite.js
+++ src/Pages/Favorite/Favorite.js
@@ -1,4 +1,4 @@
-const Home = () => {
+const Favorite = () => {
     return (
         <div>
             
@@ -6,4 +6,4 @@ const Home = () => {
     )
 }
 
-export default Home
+export default Favorite
